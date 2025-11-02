2fde5e8f8dc5741ade4821f41da46004400f1b82
Paolo Tomatis <tomatis.pol@gmail.com>
2025-11-02 21:04:54 +0100

    STYLE/FEAT: pulsante chiusura errore + testo errore input

--- client/src/components/Input.comp.tsx
+++ client/src/components/Input.comp.tsx
@@ -8,26 +8,44 @@ function Input({
     type = 'text',
     value,
     setValue,
+    error,
+    setError,
 }: {
     placeholder?: string;
     className?: string;
     type?: string;
     value: any;
     setValue: React.Dispatch<React.SetStateAction<any>>;
+    error: string;
+    setError: React.Dispatch<React.SetStateAction<string>>;
 }) {
     return (
-        <input
-            type={type}
-            placeholder={placeholder}
-            className={`${className} text-xsmall text-primary-text bg-primary-bg rounded-4xl border-[2px] px-2 py-1.5 w-[95%] max-w-[300px] focus:border-secondary`}
-            value={value}
-            onChange={(e) => {
-                const val = e.target.value;
-                setValue(
-                    type === 'number' ? (val === '' ? '' : Number(val)) : val
-                );
-            }}
-        />
+        <div className="w-[95%] max-w-[300px] flex flex-col items-center justify-center gap-0.5">
+            <input
+                type={type}
+                placeholder={placeholder}
+                className={`${className} text-xsmall text-primary-text bg-primary-bg rounded-4xl ${
+                    error ? 'border-error' : 'border-primary-text'
+                } border-[2px] px-2 py-1.5 w-full focus:border-secondary`}
+                value={value}
+                onChange={(e) => {
+                    const val = e.target.value;
+                    setValue(
+                        type === 'number'
+                            ? val === ''
+                                ? ''
+                                : Number(val)
+                            : val
+                    );
+                    if (error) {
+                        setError('');
+                    }
+                }}
+            />
+            <p className="text-error text-xsmall max-w-[90%] leading-3">
+                {error}
+            </p>
+        </div>
     );
 }
 
