fe09c3097587e34f47e4ef106670bff8c618cb59
Paolo Tomatis <tomatis.pol@gmail.com>
2025-11-02 21:06:05 +0100

    PERF: migliorato controllo token con caricamento

--- client/src/components/ProtectedRoute.comp.tsx
+++ client/src/components/ProtectedRoute.comp.tsx
@@ -1,18 +1,29 @@
 // Importazione moduli
-import { type ReactNode } from 'react';
-import { Navigate } from 'react-router-dom';
+import { type ReactNode, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { useAuth } from '../context/Auth.context';
+import Loading from './Loading.comp';
 
 // Componente dati
 function ProtectedRoute({ children }: { children: ReactNode }) {
     // Autenticazione
-    const { accessToken } = useAuth();
+    const { accessToken, loading } = useAuth();
+    // Navigatore
+    const navigator = useNavigate();
 
-    // Controllo token
+    // Controllo caricamento
+    useEffect(() => {
+        if (!loading && !accessToken) {
+            navigator('/auth');
+        }
+    }, [loading]);
+
+    // Controllo accessToken
     if (accessToken) {
         return children;
     }
-    return <Navigate to={'/auth'} replace />;
+
+    return <Loading />;
 }
 
 // Esportazione componente
