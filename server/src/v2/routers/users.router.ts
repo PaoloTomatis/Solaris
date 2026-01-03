78ef7170506cd1a981fc47fcd4d61828c175c589
Paolo Tomatis <tomatis.pol@gmail.com>
2026-01-03 15:13:20 +0100

    FEAT: creazione router esposizione rotte impostazioni utente

--- server/src/v2/routers/users.router.ts
+++ server/src/v2/routers/users.router.ts
@@ -4,12 +4,20 @@ import {
     deleteMeController,
     getMeController,
 } from '../controllers/users.controller.js';
+import {
+    getMeSettingsController,
+    patchMeSettingsController,
+} from '../controllers/usersSettings.controller.js';
 
 // Dichiarazione router
 const usersRouter = Router();
 
 // Definizione rotte
-usersRouter.get('/', getMeController).delete('/', deleteMeController);
+usersRouter
+    .get('/user-settings', getMeSettingsController)
+    .get('/', getMeController)
+    .patch('/user-settings', patchMeSettingsController)
+    .delete('/', deleteMeController);
 
 // Esportazione router
 export default usersRouter;
