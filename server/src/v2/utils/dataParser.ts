3b805faa2e516d21edc6a286939b48a2ab01427e
Paolo Tomatis <tomatis.pol@gmail.com>
2026-01-06 10:47:26 +0100

    FEAT: gestione conversione campo id (_id --> id)

--- server/src/v2/utils/dataParser.ts
+++ server/src/v2/utils/dataParser.ts
@@ -1,17 +1,39 @@
+// Firma con id
+function dataParser<T extends object, K extends keyof T>(
+    data: T,
+    removedFields: K[],
+    convertId: true
+): Omit<T, K | '_id'> & { id: string };
+
+// Firma senza id
+function dataParser<T extends object, K extends keyof T>(
+    data: T,
+    removedFields: K[],
+    convertId?: false
+): Omit<T, K>;
+
 // Funzione conversione dati
 function dataParser<T extends object, K extends keyof T>(
     data: T,
-    removedFields: K[]
+    removedFields: K[],
+    convertId?: boolean
 ) {
     // Dichiarazione dati convertiti
-    const parsedData = { ...data };
+    const parsedData = { ...data } as any;
+
+    // Controllo conversione id
+    if (convertId && parsedData['_id']) {
+        parsedData['id'] = parsedData['_id'].toString();
+        delete parsedData['_id'];
+    }
 
     // Eliminazione dati
     removedFields.forEach((key) => {
         delete parsedData[key];
     });
 
-    return parsedData as Omit<T, K>;
+    // Ritorno
+    return parsedData;
 }
 
 // Esportazione funzione
