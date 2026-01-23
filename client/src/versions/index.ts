f084e65bcb8cd273d7e834b14dd60568b72c852a
Paolo Tomatis <tomatis.pol@gmail.com>
2026-01-23 16:00:18 +0100

    FEAT: gestione versionizzazione  per seconda versione

--- client/src/versions/index.ts
+++ client/src/versions/index.ts
@@ -1,5 +1,6 @@
 // Importazione moduli
 import pagesV1 from './v1';
+import pagesV2 from './v2';
 
 // Dichiarazione versione attiva
 const ACTIVE_VERSION: number = import.meta.env.VITE_ACTIVE_VERSION;
@@ -9,7 +10,7 @@ if (!ACTIVE_VERSION || isNaN(Number(ACTIVE_VERSION)))
     throw new Error('Version environment variable is missing or is invalid');
 
 // Lista versioni
-const versions = [pagesV1];
+const versions = [pagesV1, pagesV2];
 
 // Esportazione moduli
 export { versions, ACTIVE_VERSION };
---
+++ client/src/versions/v2.tsx
@@ -0,0 +1,107 @@
+// Importazione pagine
+import Home from '../pages/global/Home.page';
+import Devices from '../pages/v2/Devices.page';
+import DeviceRegister from '../pages/v2/DeviceRegister.page';
+import Dashboard from '../pages/v2/Dashboard.page';
+import Controls from '../pages/v2/Controls.page';
+import DeviceSettings from '../pages/v2/DeviceSettings.page';
+import Log from '../pages/v2/Logs.page';
+import Stats from '../pages/global/Stats.page';
+import Page404 from '../pages/global/Page404.page';
+import Credits from '../pages/global/Credits.page';
+import UserSettings from '../pages/v2/UserSettings.page';
+import Account from '../pages/v2/Account.page';
+import Auth from '../pages/v2/Auth.page';
+import Warning from '../pages/global/Warning.page';
+import Parent from '../components/global/Parent.comp';
+import ProtectedRoute from '../components/v2/ProtectedRoute.comp';
+
+// Dichiarazione pagine router
+const pagesv2 = [
+    { path: '/warning', element: <Warning /> },
+    {
+        element: <Parent />,
+        children: [
+            { path: '/', element: <Home /> },
+            {
+                path: '/devices',
+                element: (
+                    <ProtectedRoute>
+                        <Devices />
+                    </ProtectedRoute>
+                ),
+            },
+            {
+                path: '/devices/add',
+                element: (
+                    <ProtectedRoute>
+                        <DeviceRegister />
+                    </ProtectedRoute>
+                ),
+            },
+            {
+                path: '/dashboard/:id',
+                element: (
+                    <ProtectedRoute>
+                        <Dashboard />
+                    </ProtectedRoute>
+                ),
+            },
+            {
+                path: '/dashboard/:id/controls',
+                element: (
+                    <ProtectedRoute>
+                        <Controls />
+                    </ProtectedRoute>
+                ),
+            },
+            {
+                path: '/dashboard/:id/settings',
+                element: (
+                    <ProtectedRoute>
+                        <DeviceSettings />
+                    </ProtectedRoute>
+                ),
+            },
+            {
+                path: '/dashboard/:id/log',
+                element: (
+                    <ProtectedRoute>
+                        <Log />
+                    </ProtectedRoute>
+                ),
+            },
+            {
+                path: '/dashboard/:id/stats',
+                element: (
+                    <ProtectedRoute>
+                        <Stats />
+                    </ProtectedRoute>
+                ),
+            },
+            { path: '/credits', element: <Credits /> },
+            {
+                path: '/settings',
+                element: (
+                    <ProtectedRoute>
+                        <UserSettings />
+                    </ProtectedRoute>
+                ),
+            },
+            {
+                path: '/account',
+                element: (
+                    <ProtectedRoute>
+                        <Account />
+                    </ProtectedRoute>
+                ),
+            },
+            { path: '/auth/:type', element: <Auth /> },
+            { path: '/auth', element: <Auth /> },
+            { path: '*', element: <Page404 /> },
+        ],
+    },
+];
+
+// Esportazione pagine router
+export default pagesv2;
