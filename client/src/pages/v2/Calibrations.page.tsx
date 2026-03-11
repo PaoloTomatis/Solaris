b1d01a3b07a1a500bb8a29bfabfa0266338775b7
Paolo Tomatis <tomatis.pol@gmail.com>
2026-03-11 16:28:02 +0100

    FIX: correzione ricezione evento misurazione + visualizzazione calibrazione nella apposita pagina

--- client/src/pages/v2/Calibrations.page.tsx
+++ client/src/pages/v2/Calibrations.page.tsx
@@ -144,7 +144,7 @@ function Calibrations() {
                     // Invio notifica
                     notify(
                         'MISURAZIONE',
-                        `Umidità Interna: ${eventData?.measurement?.humI?.toFixed(1)}% | Luminosità: ${eventData?.measurement?.lum?.toFixed(1)}% | Umidità Esterna: ${eventData?.measurement?.humE}% | Temperatura: ${eventData?.measurement?.temp}°C`,
+                        `Umidità Interna: ${eventData?.measurements?.humI?.toFixed(1)}% | Luminosità: ${eventData?.measurements?.lum?.toFixed(1)}% | Umidità Esterna: ${eventData?.measurements?.humE}% | Temperatura: ${eventData?.measurements?.temp}°C`,
                         'success',
                     );
                 }),
@@ -168,7 +168,24 @@ function Calibrations() {
                 }),
             );
 
-            // Iscrizione evento stato
+            // Iscrizione evento calibrazione
+            unsubscribes.push(
+                ws.subscribe(deviceId, 'calibration', (eventData: any) => {
+                    console.log('CALIBRAZIONE');
+                    console.log(eventData);
+                    // Impostazione impostazioni dispositivo
+                    setSettings((prev) => {
+                        return prev
+                            ? {
+                                  ...prev,
+                                  [eventData.sensor]: eventData.measurement,
+                              }
+                            : null;
+                    });
+                }),
+            );
+
+            // Iscrizione evento errore
             unsubscribes.push(
                 ws.subscribe(deviceId, 'error', (eventData: any) => {
                     // Impostazione errore
